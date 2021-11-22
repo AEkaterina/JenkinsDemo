b8f4ec47528ece9bed7eba4664bc3ad1ca923fac
Astapkina Ekaterina <33911738+AEkaterina@users.noreply.github.com>
2021-11-22 13:28:10 +0300

    added settings.json

--- test/src/test/java/JenkinsTest.java
+++ test/src/test/java/JenkinsTest.java
@@ -1,4 +1,5 @@
 import aquality.selenium.browser.AqualityServices;
+import aquality.selenium.browser.Browser;
 import aquality.selenium.core.logging.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,9 +9,17 @@ public class JenkinsTest {
     @Test
     public void test() {
         Logger logger = AqualityServices.getLogger();
+
+        Browser browser = AqualityServices.getBrowser();
+        browser.maximize();
+        browser.goTo("https://wikipedia.org");
+        browser.waitForPageToLoad();
+
         final String TEXT = "Hello World!";
 
         Assert.assertEquals(TEXT, "Hello World!", "Texts don't match");
         logger.info("Test is done");
+
+        browser.quit();
     }
 }
